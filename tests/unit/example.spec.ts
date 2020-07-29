1c198ead1fd8e14403e9e9376ce33eeb46a8f2a7
Kenjiro Mimura <3bMnLtlllpN@protonmail.com>
2020-07-29 19:46:42 +0900

    add a fail test

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -2,6 +2,13 @@ import { shallowMount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
+  it('fail', () => {
+    const msg = 'new message'
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg }
+    })
+    expect(false).toBe(true);
+  })
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
